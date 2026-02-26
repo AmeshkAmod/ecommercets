a828d5bf28884fce60eeee81a235629e45d48d5c
githuuubo <arjuntk41155@gmail.com>
2026-02-26 11:07:08 +0530

    Update ProtectedRoutes.tsx

--- frontend/src/routes/ProtectedRoutes.tsx
+++ frontend/src/routes/ProtectedRoutes.tsx
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import { Navigate } from "react-router-dom";
 import { ReactNode } from "react";
 import { useAppSelector } from "../store/hooks";
@@ -12,12 +11,4 @@ export default function ProtectedRoute({
 }:ProtectedRouteProps) {
   const { isAuthenticated } = useAppSelector((s) => s.auth);
   return isAuthenticated ? <>{children}</> : <Navigate to="/login" replace/>;
-=======
-import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
-
-export default function ProtectedRoute({ children }) {
-  const { isAuthenticated } = useSelector((s) => s.auth);
-  return isAuthenticated ? children : <Navigate to="/login" />;
->>>>>>> Stashed changes
 }
