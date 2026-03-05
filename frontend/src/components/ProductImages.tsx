b4fafdbaa02e81ffce6e4893e9df072b70e0e45d
AmeshkAmod <ameshamod1@gmail.com>
2026-03-05 11:48:16 +0530

    multiple image

--- frontend/src/components/ProductImages.tsx
+++ frontend/src/components/ProductImages.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import type { Product } from "../types/product";
 
 interface ProductImagesProps {
@@ -5,25 +6,41 @@ interface ProductImagesProps {
 }
 
 export default function ProductImages({ product }: ProductImagesProps) {
+  const [selected, setSelected] = useState(0)
 
-  let imageUrl = "";
+  const images = product.images || [];
 
-  if (product.images && product.images.length > 0) {
-    imageUrl = product.images[0];
-  } else if (Array.isArray(product.image)) {
-    imageUrl = product.image[0]
-  } else {
-    imageUrl = product.images;
+  if (images.length === 0) {
+    return (
+      <div className="bg-[#020617] border border-gray-800 rounded-xl p-4">
+        No image
+      </div>
+    )
   }
 
 
   return (
-    <div className="bg-[#020617] border border-gray-800 rounded-xl p-4 flex items-center justify-center">
-      <img
-        src={imageUrl}
-        alt={product.title}
-        className="max-h-80 object-contain"
-      />
+    <div className="flex gap-4">
+
+      {/* Thumbnails */}
+      <div>
+        {images.map((img, i) => (
+          <img
+            key={i}
+             onClick={() => setSelected(i)}
+            className={`w-16 h-16 object-cover cursor-pointer border rounded ${selected ===i ? "border-yellow-400" : "border-gray-700"}`}
+          />
+        ))}
+      </div>
+
+      {/* Main image */} 
+      <div className="bg-[#020617] border border-gray-800 rounded-xl p-4 flex items-center justify-center">
+        <img
+          src={images[selected]}
+          alt={product.title}
+          className="max-h-96 object-contain"
+        />
+      </div>
     </div>
   );
 }
